a6a3e15ad8200b6c8a373116c2e1bcc4bfdea5bd
Daniel Amaya Marin <daniel.amayam@hotmail.com>
2024-04-04 23:12:53 -0500

    Estructura inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <p className="text-red-800">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
